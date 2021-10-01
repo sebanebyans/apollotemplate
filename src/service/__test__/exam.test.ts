10a0187a73cd78ba5eb300aa7c5363297a01eb51
Seba <s.elgueda@outlook.com>
2021-10-01 18:55:51 -0300

    fix(eslint) add eslint-plugin-jest plugin

--- src/service/__test__/exam.test.ts
+++ src/service/__test__/exam.test.ts
@@ -1,69 +1,64 @@
+/* eslint-disable no-underscore-dangle */
 import { ExamModel } from '../../database/models/exam';
 import examMock from '../../utils/test-utils/examMock.json';
 import { dbConnect, dbDisconnect } from '../../utils/test-utils/dbhandler';
-import {search} from '../exam';
+import { search } from '../exam';
 
 beforeAll(async () => dbConnect());
 afterAll(async () => dbDisconnect());
 
 describe('exam Model Test Suite', () => {
-
-  
   test('should validate exam Schema', async () => {
-    let exam = new ExamModel({
+    const exam = new ExamModel({
       ...examMock.invalid,
     });
-    expect(async () => await exam.save()).rejects.toThrow(
+    expect(async () => exam.save()).rejects.toThrow(
       'Exam validation failed: title: Path `title` is required.'
     );
   });
 
   test('should validate saving a new exam ', async () => {
-    let exam = new ExamModel({
+    const exam = new ExamModel({
       ...examMock.valid2,
     });
     const newExam = await exam.save();
+    // eslint-disable-next-line no-underscore-dangle
     expect(newExam._id).toBeDefined();
   });
 
   test('should validate not saving exams with same code ', async () => {
-    let exam = new ExamModel({
+    const exam = new ExamModel({
       ...examMock.valid,
     });
 
     await expect(async () => {
-      const newExam = await exam.save();
+      await exam.save();
     }).rejects.toThrowError(/duplicate/);
   });
 
   test('should validate update exams', async () => {
-    examMock.valid.code = "999";
-    let exam = new ExamModel({
+    examMock.valid.code = '999';
+    const exam = new ExamModel({
       ...examMock.valid,
     });
     const newExam = await exam.save();
-    newExam.price = 999;    
-    const updatedExam = await ExamModel.findByIdAndUpdate(newExam._id, exam,{ new: true });
+    newExam.price = 999;
+    const updatedExam = await ExamModel.findByIdAndUpdate(newExam._id, exam, { new: true });
     expect(updatedExam.price).toEqual(999);
   });
 
   test('should validate get highlights exams', async () => {
-  
-    const highlight = await ExamModel.find({'highlight':true});     
+    const highlight = await ExamModel.find({ highlight: true });
     expect(highlight.length).toEqual(2);
   });
 
   test('should validate search by pne patters', async () => {
-
-    const examResults = await search("sangre")
+    const examResults = await search('sangre');
     expect(examResults.length).toEqual(1);
   });
 
   test('should validate search by multiples patters', async () => {
-
-    const examResults = await search("pcr")
+    const examResults = await search('pcr');
     expect(examResults.length).toEqual(2);
   });
-
- 
 });
